78baf42968de6ca722ecc033dea0701d5ac7409c
Willian Krause <williankrause@MacBook-Pro-de-Willian.local>
2019-09-13 08:43:44 -0300

    Updates

---
+++ precache-manifest.21e39837dfdbd936fd6d0c016469f43b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fc91c6a9c9910c867cc",
+    "url": "/static/js/app.445df871.chunk.js"
+  },
+  {
+    "revision": "f16959773581a57813f0",
+    "url": "/static/js/runtime~app.91065de9.js"
+  },
+  {
+    "revision": "8beab50a49a684ca4cd9",
+    "url": "/static/js/2.e45235d4.chunk.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "c7578911196974808febf223f05a8364",
+    "url": "/static/media/robot-prod.c7578911.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "761a29cc5b9163dd2a23118c3d85605d",
+    "url": "/static/media/expo-icon.761a29cc.png"
+  },
+  {
+    "revision": "54da1e9816c77e30ebc5920e256736f2",
+    "url": "/static/media/robot-dev.54da1e98.png"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8c8cdfd9ab7ff1995a08f75467282e51",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "8a9b5d9bb19512ae8952b992b07cb63e",
+    "url": "/index.html"
+  }
+];
